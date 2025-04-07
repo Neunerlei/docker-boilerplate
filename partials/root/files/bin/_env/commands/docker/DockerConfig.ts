c807aeee4ca22af1ddf7e94be746ac5b5d9c8f6a
Neunerlei <code@neunerlei.eu>
2025-04-07 18:04:57 +0200

    feat: automatically detect port for project host

--- partials/root/files/bin/_env/commands/docker/DockerConfig.ts
+++ partials/root/files/bin/_env/commands/docker/DockerConfig.ts
@@ -9,9 +9,12 @@ export class DockerConfig {
     public readonly projectProtocol: string = getEnvValue('DOCKER_PROJECT_PROTOCOL', 'http');
     public readonly projectDomain: string = getEnvValue('DOCKER_PROJECT_DOMAIN', 'localhost');
     public readonly projectIp: string = getEnvValue('DOCKER_PROJECT_IP', '127.0.0.1');
+    public readonly projectPort: string = getEnvValue('DOCKER_PROJECT_PORT', '80');
     public readonly shellsToUse: string[] = getEnvValue('SHELLS_TO_USE', 'bash,sh,zsh,dash,ksh').split(',').map((shell) => shell.trim());
 
     public get projectHost(): string {
-        return `${this.projectProtocol}://${this.projectDomain}`;
+        const expectedPort = this.projectProtocol === 'http' ? '80' : '443';
+        const port = this.projectPort === expectedPort ? '' : `:${this.projectPort}`;
+        return `${this.projectProtocol}://${this.projectDomain}${port}`;
     }
 }
